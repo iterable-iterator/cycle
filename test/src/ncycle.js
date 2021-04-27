6c97b839d54f073378ab8b3c34e0483d413c7288
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-04-27 16:27:57 +0200

    :microscope: test: Increase coverage to 100%.

---
+++ test/src/ncycle.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+
+import {ncycle} from '../../src/index.js';
+
+test('empty input', (t) => {
+	const it = ncycle([], 10000);
+	t.true(it.next().done);
+});
+
+test('string input', (t) => {
+	const actual = Array.from(ncycle('AB', 3));
+	const expected = ['A', 'B', 'A', 'B', 'A', 'B'];
+	t.deepEqual(actual, expected);
+});
